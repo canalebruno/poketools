50a72dbcec487778bec271cb0ee081e8767b639d
canalebruno <b.canalemedeiros@gmail.com>
2023-01-14 23:32:21 +1100

    nuzlocke hook updated

--- hooks/useNuzlocke.tsx
+++ hooks/useNuzlocke.tsx
@@ -1,10 +1,7 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useState,
-  useEffect,
-} from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+import { Pokemon } from "../utils/Interfaces";
+import nuzlockeJson from "../json/nuzlocke.json";
+import pokedex from "../json/nationalDex.json";
 
 interface NuzlockeProviderProps {
   children: ReactNode;
@@ -17,6 +14,14 @@ interface NuzlockeContextData {
   setCustomOptions: (value: string[]) => void;
   typeSelected: string;
   setTypeSelected: (value: string) => void;
+  handleGenerateNuzlockeHunt: () => void;
+  hunt: Hunt[];
+}
+
+interface Hunt {
+  id: number;
+  name: string;
+  pokemon: Pokemon | undefined;
 }
 
 const NuzlockeContext = createContext<NuzlockeContextData>(
@@ -27,6 +32,41 @@ export function NuzlockeProvider({ children }: NuzlockeProviderProps) {
   const [gameExclusive, setGameExclusive] = useState(["tradable"]);
   const [customOptions, setCustomOptions] = useState(["repeat"]);
   const [typeSelected, setTypeSelected] = useState("all");
+  const [hunt, setHunt] = useState([] as Hunt[]);
+
+  function handleGenerateNuzlockeHunt() {
+    const huntGenerator = nuzlockeJson
+      .sort((a, b) => {
+        return a.id - b.id;
+      })
+      .map((loc) => {
+        return {
+          id: loc.id,
+          name: loc.name,
+          pokemon: randomPokemon([
+            ...loc.general,
+            ...loc.violet,
+            ...loc.scarlet,
+          ]),
+        };
+      });
+
+    setHunt(huntGenerator);
+  }
+
+  function randomPokemon(options: string[]) {
+    const quantity = options.length;
+
+    const randomIndex = Math.floor(Math.random() * quantity);
+
+    const randomPokemon = options[randomIndex];
+
+    const pokemon = pokedex.find((pkmn) => {
+      return pkmn.id === randomPokemon;
+    });
+
+    return pokemon;
+  }
 
   return (
     <NuzlockeContext.Provider
@@ -37,6 +77,8 @@ export function NuzlockeProvider({ children }: NuzlockeProviderProps) {
         setCustomOptions,
         typeSelected,
         setTypeSelected,
+        handleGenerateNuzlockeHunt,
+        hunt,
       }}
     >
       {children}
--- pages/nuzlocke.tsx
+++ pages/nuzlocke.tsx
@@ -1,54 +1,12 @@
 import styles from "../styles/Nuzlocke.module.scss";
-import nuzlockeJson from "../json/nuzlocke.json";
-import pokedex from "../json/nationalDex.json";
-import { useState } from "react";
-import { Pokemon } from "../utils/Interfaces";
+
 import LocationCard from "../components/LocationCard";
 import NuzlockeFilterControl from "../components/NuzlockeFilterControl";
 import Button from "@mui/material/Button";
-
-interface Hunt {
-  id: number;
-  name: string;
-  pokemon: Pokemon | undefined;
-}
+import { useNuzlocke } from "../hooks/useNuzlocke";
 
 export default function HomeBoxes() {
-  const [hunt, setHunt] = useState([] as Hunt[]);
-
-  function handleGenerateNuzlockHunt() {
-    const huntGenerator = nuzlockeJson
-      .sort((a, b) => {
-        return a.id - b.id;
-      })
-      .map((loc) => {
-        return {
-          id: loc.id,
-          name: loc.name,
-          pokemon: randomPokemon([
-            ...loc.general,
-            ...loc.violet,
-            ...loc.scarlet,
-          ]),
-        };
-      });
-
-    setHunt(huntGenerator);
-  }
-
-  function randomPokemon(options: string[]) {
-    const quantity = options.length;
-
-    const randomIndex = Math.floor(Math.random() * quantity);
-
-    const randomPokemon = options[randomIndex];
-
-    const pokemon = pokedex.find((pkmn) => {
-      return pkmn.id === randomPokemon;
-    });
-
-    return pokemon;
-  }
+  const { handleGenerateNuzlockeHunt, hunt } = useNuzlocke();
 
   return (
     <div className={styles.container}>
@@ -56,7 +14,7 @@ export default function HomeBoxes() {
       <Button
         sx={{ marginTop: "2rem", fontSize: "1.25rem" }}
         variant="contained"
-        onClick={handleGenerateNuzlockHunt}
+        onClick={handleGenerateNuzlockeHunt}
       >
         Generate
       </Button>
