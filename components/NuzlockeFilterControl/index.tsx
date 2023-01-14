e1a37cb8ab9bab1c4d5a273e9eec70b78946e22d
canalebruno <b.canalemedeiros@gmail.com>
2023-01-14 23:12:04 +1100

    filter control done

--- components/NuzlockeFilterControl/index.tsx
+++ components/NuzlockeFilterControl/index.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { usePokedex } from "../../hooks/usePokedex";
+import { useState } from "react";
 import styles from "./styles.module.scss";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -7,8 +6,6 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import VisibilityTwoToneIcon from "@mui/icons-material/VisibilityTwoTone";
-import VisibilityOffTwoToneIcon from "@mui/icons-material/VisibilityOffTwoTone";
 
 interface FilterControlProps {
   vertical?: boolean;
@@ -18,6 +15,8 @@ export default function NuzlockeFilterControl({
   vertical = false,
 }: FilterControlProps) {
   const [gameExclusive, setGameExclusive] = useState(["tradable"]);
+  const [customOptions, setCustomOptions] = useState(["repeat"]);
+  const [typeSelected, setTypeSelected] = useState("all");
 
   function handleExclusivityValues(
     event: React.MouseEvent<HTMLElement>,
@@ -26,14 +25,19 @@ export default function NuzlockeFilterControl({
     setGameExclusive(newValues);
   }
 
+  function handleCustomValues(
+    event: React.MouseEvent<HTMLElement>,
+    newValues: string[]
+  ) {
+    setCustomOptions(newValues);
+  }
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setTypeSelected(event.target.value as string);
+  };
+
   return (
     <div className={styles.container}>
-      <ul>
-        <li>repeat</li>
-        <li>basic</li>
-        <li>type</li>
-        <li>new gen</li>
-      </ul>
       <ToggleButtonGroup
         orientation={vertical ? "vertical" : "horizontal"}
         exclusive
@@ -51,6 +55,53 @@ export default function NuzlockeFilterControl({
           Trade Exclusives
         </ToggleButton>
       </ToggleButtonGroup>
+      <ToggleButtonGroup
+        orientation={vertical ? "vertical" : "horizontal"}
+        value={customOptions}
+        onChange={handleCustomValues}
+        aria-label="game exclusive"
+      >
+        <ToggleButton value="basic" aria-label="only first stage pokémon">
+          Only First Stage
+        </ToggleButton>
+        <ToggleButton value="repeat" aria-label="repeat pokémon species">
+          Repeat Species
+        </ToggleButton>
+        <ToggleButton value="newGen" aria-label="only new generation pokémon">
+          Only New Gen
+        </ToggleButton>
+      </ToggleButtonGroup>
+      <FormControl sx={{ minWidth: 120 }}>
+        <InputLabel id="type-select-label">Type</InputLabel>
+        <Select
+          labelId="type-select-label"
+          id="type-select"
+          value={typeSelected}
+          label="Type"
+          onChange={handleChange}
+        >
+          <MenuItem value={"all"}>All</MenuItem>
+          <MenuItem value={"random"}>Random</MenuItem>
+          <MenuItem value={"bug"}>Bug</MenuItem>
+          <MenuItem value={"dark"}>Dark</MenuItem>
+          <MenuItem value={"dragon"}>Dragon</MenuItem>
+          <MenuItem value={"electric"}>Electric</MenuItem>
+          <MenuItem value={"fairy"}>Fairy</MenuItem>
+          <MenuItem value={"fighting"}>Fighting</MenuItem>
+          <MenuItem value={"fire"}>Fire</MenuItem>
+          <MenuItem value={"flying"}>Flying</MenuItem>
+          <MenuItem value={"ghost"}>Ghost</MenuItem>
+          <MenuItem value={"grass"}>Grass</MenuItem>
+          <MenuItem value={"ground"}>Ground</MenuItem>
+          <MenuItem value={"ice"}>Ice</MenuItem>
+          <MenuItem value={"normal"}>Normal</MenuItem>
+          <MenuItem value={"poison"}>Poison</MenuItem>
+          <MenuItem value={"psychic"}>Psychic</MenuItem>
+          <MenuItem value={"rock"}>Rock</MenuItem>
+          <MenuItem value={"steel"}>Steel</MenuItem>
+          <MenuItem value={"water"}>Water</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 }
