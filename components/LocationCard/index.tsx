6f2b6c66125024cb92f87c3bb8577e3363635a21
canalebruno <b.canalemedeiros@gmail.com>
2023-01-15 01:02:41 +1100

    empty card

--- components/LocationCard/index.tsx
+++ components/LocationCard/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Pokemon } from "../../utils/Interfaces";
 import { handleName } from "../../utils/NameFormatting";
 import styles from "./styles.module.scss";
+import GrassIcon from "@mui/icons-material/Grass";
 
 interface LocationCardProps {
   name: string;
@@ -12,18 +13,25 @@ export default function LocationCard({ name, pokemon }: LocationCardProps) {
   return (
     <div className={styles.container}>
       <div className={styles.title}>{name}</div>
-      {pokemon && (
-        <div className={styles.pokemon}>
-          <Image
-            unoptimized
-            src={`svicons/${pokemon.icon}`}
-            alt={name}
-            width={80}
-            height={80}
-          />
-          <span>{handleName(pokemon, false, "Paldean", true)}</span>
-        </div>
-      )}
+      <div className={styles.pokemon}>
+        {pokemon ? (
+          <>
+            <Image
+              unoptimized
+              src={`svicons/${pokemon.icon}`}
+              alt={name}
+              width={80}
+              height={80}
+            />
+            <span>{handleName(pokemon, false, "Paldean", true)}</span>
+          </>
+        ) : (
+          <>
+            <GrassIcon />
+            <span>Nothing to catch in this area</span>
+          </>
+        )}
+      </div>
     </div>
   );
 }
