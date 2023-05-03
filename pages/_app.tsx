a5472c146b4d648b96caf2c08310370c8a55b77c
canalebruno <b.canalemedeiros@gmail.com>
2023-05-03 20:02:43 +1000

    fix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,17 +34,20 @@ export default function App({ Component, pageProps }: AppProps) {
       <WindowSizeProvider>
         <PokedexProvider>
           <ShinyTrackerProvider>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
-            <script>
-              {`
+            <Head>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+              ></script>
+              <script>
+                {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}')
               `}
-            </script>
-            <Head>         
+              </script>
               <title>Pokémon Tools</title>
             </Head>
             <main>
